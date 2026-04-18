7d787cdf4896c53b062b187232bcaa1446382846
Laxmi Priya <pendyalapriya11@gmail.com>
2026-04-18 23:06:43 +0530

    added local storage to count variable

--- javaScriptFile.js
+++ javaScriptFile.js
@@ -1,24 +1,30 @@
-let count = 0;
+let count = localStorage.getItem("count");
+if(count===null)count=0;
+else count = Number(count);
+
     let counter = document.querySelector('.counter');
     let incbtn = document.getElementById('inc');
     let decbtn = document.getElementById('dec');
     let resetbtn = document.getElementById('reset');
-
+    updateColor();
     incbtn.addEventListener('click', ()=> {
       count++;
-      updateColor(count);
+      updateColor();
     });
     decbtn.addEventListener('click', ()=> {
       if(count>0) count--;
-      updateColor(count);
+      updateColor();
     });
     resetbtn.addEventListener('click', ()=> {
       count = 0;
-      updateColor(count);
+      updateColor();
     });
 
-    function updateColor(count) {
+    function updateColor() {
       counter.innerText = count;
+
+      localStorage.setItem("count", count);
+      
       if(count>0) {
         counter.style.color = "green";
       }
